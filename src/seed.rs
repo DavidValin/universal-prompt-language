aa98a135d0a72b1fb0224da69842ee481b23a656
David Valin <hola@davidvalin.com>
2026-08-09 16:54:01 -0500

    Updated default upl prompts. New previews

--- src/seed.rs
+++ src/seed.rs
@@ -18,12 +18,22 @@ use crate::repository::protocol::upl_home;
 // when writing it to `~/.upl/prompts/`.
 const SAMPLES: &[(&str, &str)] = &[
     ("analyze_argument.txt", include_str!("../samples/analyze_argument.txt")),
+    ("analyze_psychological_facts.txt", include_str!("../samples/analyze_psychological_facts.txt")),
+    ("brainstorm_ideas.txt", include_str!("../samples/brainstorm_ideas.txt")),
+    ("cognitive_behavioral_exercise.txt", include_str!("../samples/cognitive_behavioral_exercise.txt")),
+    ("career_development_plan.txt", include_str!("../samples/career_development_plan.txt")),
     ("create_a_plan.txt", include_str!("../samples/create_a_plan.txt")),
     ("create_rest_api.txt", include_str!("../samples/create_rest_api.txt")),
     ("explain_subject.txt", include_str!("../samples/explain_subject.txt")),
+    ("financial_plan.txt", include_str!("../samples/financial_plan.txt")),
+    ("improve_personal_finances.txt", include_str!("../samples/improve_personal_finances.txt")),
     ("implement_user_story.txt", include_str!("../samples/implement_user_story.txt")),
+    ("marketing_campaign.txt", include_str!("../samples/marketing_campaign.txt")),
+    ("monthly_food_plan.txt", include_str!("../samples/monthly_food_plan.txt")),
     ("review_article.txt", include_str!("../samples/review_article.txt")),
+    ("review_contract.txt", include_str!("../samples/review_contract.txt")),
     ("teach_foundations.txt", include_str!("../samples/teach_foundations.txt")),
+    ("wellness_plan.txt", include_str!("../samples/wellness_plan.txt")),
 ];
 
 // The pre-built tag store (bincode). Its associations reference the sha256 of
